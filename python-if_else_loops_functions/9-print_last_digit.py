57f9e076a644516a6dca185ee9ce041cea74a071
juanRCoder <guillecarck@gmail.com>
2023-07-11 11:10:53 -0700

    last_digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
+    # abs / valor absoluto de un numero sin importar + o -
+    last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
