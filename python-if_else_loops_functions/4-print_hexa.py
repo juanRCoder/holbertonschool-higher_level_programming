96038eedf6ff64cbc361a178f36359b9fc89e381
juanRCoder <guillecarck@gmail.com>
2023-07-11 09:01:34 -0700

    print numbers format hexadecimal

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# :x / imprime numeros en hexa minusculas en un string format
 string = "{} = 0x{:x}"
-for number in range(98):
+for number in range(99):
     print(string.format(number, number))
