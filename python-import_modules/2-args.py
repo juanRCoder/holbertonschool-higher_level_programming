ba97add63eb1741f86be45e15c6bbcda28043901
juanRCoder <guillecarck@gmail.com>
2023-07-12 15:20:57 -0700

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,16 +2,12 @@
 from sys import argv
 if __name__ == '__main__':
     argc = len(argv) - 1
-
     if argc == 0:
         print("{} arguments".format(argc))
-
     elif argc == 1:
         print("{} argument:".format(argc))
-
     else:
         print("{} arguments: ".format(argc))
-
-    if argc  >= 1:
+    if argc >= 1:
         for i in range(1, argc + 1):
             print("{}: {}".format(i, argv[i]))
