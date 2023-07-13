84bfefb7f7316b358c6b5cd8da5c5aa44566a057
juanRCoder <guillecarck@gmail.com>
2023-07-12 17:32:58 -0700

    task0: print numbers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    str_len = len(my_list)
-    for i in range(str_len):
+    for i in range(len(my_list)):
         print("{}".format(my_list[i]))
