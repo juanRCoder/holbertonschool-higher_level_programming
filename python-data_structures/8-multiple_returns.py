3333f676ad7c26ecb904b4cab1086857fe807638
juanRCoder <guillecarck@gmail.com>
2023-07-14 17:05:35 -0700

    task8: return length and first character

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     len_tuple = len(sentence)
     first_character = sentence[0]
-    if not len_tuple:
+    if len_tuple <= 0:
         fisrt_character = None
     return len_tuple, first_character
