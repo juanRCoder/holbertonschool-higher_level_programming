64d79db0b1f6109533dc3305c6caa1c702e5e73e
juanRCoder <guillecarck@gmail.com>
2023-07-15 09:55:12 -0700

    task3: list reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        continue
+        pass
     else:
         my_list.reverse()
         for i in range(len(my_list)):
