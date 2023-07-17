da9a77aa39736591348b7b856b2852d1ec1935b0
juanRCoder <guillecarck@gmail.com>
2023-07-17 16:43:49 -0700

    task12: numbers romans

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string == None:
+        return 0
+    numbers_romans = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'W' : 1000
+            }
+    int_value = 0
+    prev_value = 0
+
+    for roman_symbol in reversed(roman_string):
+        value = numbers_romans.get(roman_symbol, 0)
+    if value < prev_value:
+        int_value -= value
+    else:
+        int_value += value
+    prev_value = value
+    return int_value
