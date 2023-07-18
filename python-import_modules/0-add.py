7c62a1d8bfc6a37f662d1087e6ca297f659ab73d
juanRCoder <guillecarck@gmail.com>
2023-07-18 13:52:14 -0700

    proyect02: import & modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    resultado = add(a, b)
-    print("{} + {} = {}".format(a, b, resultado))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
