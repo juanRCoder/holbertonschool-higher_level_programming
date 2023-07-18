a863da6cf31e13c9d6dbb957c3be7f64702f7ca9
juanRCoder <guillecarck@gmail.com>
2023-07-18 15:00:01 -0700

    task01: int number or not

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int(value):
-            print("{:d}".format(value))
-            return True
-    except ValueError:
-        pass
-    return False
+        print("{:d}".format(value))
+    except:
+        return False
+    else:
+        return True
