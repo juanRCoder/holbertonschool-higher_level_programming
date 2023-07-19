7060adbd2d674153261b11edb0f682585600b6f5
juanRCoder <guillecarck@gmail.com>
2023-07-19 14:30:40 -0700

    task1: attribute(priv) - size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square:
     """Class Square that defines a square.
     """
     def __init__(self, size):
-        """Initialize method that stores the size of the square
-            attribute1(priv): size 
+        """ Initialize method that stores the size of the square
+            attribute1(priv): size
         """
         self.__size = size
