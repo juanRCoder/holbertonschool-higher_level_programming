055d2f86ec224fbe7d08320ffd1e5786fa8e52e8
juanRCoder <guillecarck@gmail.com>
2023-07-19 15:07:26 -0700

    task2: square conditionales

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,14 +6,14 @@ Defines a square class.
 
 
 class Square:
-        """Class Square that defines a square.
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Initialize method that stores the size of the square
         """
-        def __init__(self, size=0):
-            """Initialize method that stores the size of the square
-            """
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = int(size)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
