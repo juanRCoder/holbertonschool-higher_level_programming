20a4f1720cc689a501f916f9328a32fe9bfaa53e
juanRCoder <guillecarck@gmail.com>
2023-07-19 16:55:45 -0700

    task04: square

---
+++ python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Defines a square class.
+"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Initialize method that stores the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """method area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """method to returns the size value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method to set the size value of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
