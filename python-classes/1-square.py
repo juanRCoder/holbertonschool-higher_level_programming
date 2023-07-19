34b6c64817a71150eac0b0faf4f5bbf49acb6533
juanRCoder <guillecarck@gmail.com>
2023-07-19 14:28:38 -0700

    task1: attribute (priv)

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Defines a square class.
+"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+            attribute1(priv): size 
+        """
+        self.__size = size
