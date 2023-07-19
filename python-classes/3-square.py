bc45285dfeb174992242cb60c3da8bad22f90f75
juanRCoder <guillecarck@gmail.com>
2023-07-19 16:00:25 -0700

    task03: square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = int(size)
+
     def area(self):
         """method area of the square
         """
