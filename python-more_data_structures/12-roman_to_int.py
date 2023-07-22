b97e756706326b3b70d0a507c67e3bbdff35dc48
juanRCoder <guillecarck@gmail.com>
2023-07-21 20:53:57 -0700

    task12: romans int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    dictionary_romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dictionary_romans = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     acumulador = 0
     comparador = 0
 
