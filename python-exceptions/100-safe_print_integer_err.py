620fd663bf8a74f0f17724e32ba2e85553b13a67
juanRCoder <guillecarck@gmail.com>
2023-07-21 21:17:21 -0700

    task100: print int-err

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 import sys
 def safe_print_integer_err(value):
     try:
-        print("{}".format(value))
+        print("{:d}".format(value))
     except (ValueError, TypeError) as e:
         print("Exception:", e, file=sys.stderr)
         return False
