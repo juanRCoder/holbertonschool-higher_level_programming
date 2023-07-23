1977f383babf4eaedabf5fa41adb5c4d2cb78a3f
juanRCoder <guillecarck@gmail.com>
2023-07-23 15:27:43 -0700

    task02: Splitting elements of an array

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module is composed by a function that divides the numbers of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """This function divides all the elements of an array
+    """
+    messageError = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(messageError)
+
+    len_row = len(matrix[0])
+    for rows in matrix:
+        if not rows or not isinstance(rows, list):
+            raise TypeError(mesageError)
+
+        if len(rows) != len_row:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = list(
+            map(lambda row: list(
+                map(lambda element: round(element / div, 2), row)
+                ), matrix)
+            )
+    return new_matrix
