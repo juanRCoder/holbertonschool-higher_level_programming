892e8aa09b6b9cd6410400026f2ab21dded4e198
juanRCoder <guillecarck@gmail.com>
2023-07-22 21:41:26 -0700

    task03: print name and last_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,6 +12,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        return ("My name is {} {}".format(first_name, last_name))
+        return print("My name is {} {}".format(first_name, last_name))
     else:
-        return ("My name is {}".format(first_name))
+        return print("My name is {}".format(first_name))
