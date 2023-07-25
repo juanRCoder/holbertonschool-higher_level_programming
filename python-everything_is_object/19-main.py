9bb449015dfcd8605c2135e5ab8865f43900fcb0
juanRCoder <guillecarck@gmail.com>
2023-07-25 14:46:54 -0700

    proyect10: everything_is_object

---
+++ python-everything_is_object/19-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_lis)
+
+new_list = copy_list(my_lis)
+
+print(my_lis)
+print(new_lis)
+
+print(new_list == my_lis)
+print(new_list is my_lis)
