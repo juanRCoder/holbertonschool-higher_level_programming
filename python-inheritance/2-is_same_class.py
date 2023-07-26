c6fb45c615da1042aef4939cb8e42ce06e278ef7
juanRCoder <guillecarck@gmail.com>
2023-07-26 15:21:44 -0700

    task02: type of data

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""This module has a function what return the type of data
+"""
+
+
 def is_same_class(obj, a_class):
     """Function that returns True/False if obj is a type of a_class
     """
-
-
     return type(obj) is a_class
