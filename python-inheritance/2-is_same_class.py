fd0c48d0d24c76d3b9c8466342f3c9d60e850157
juanRCoder <guillecarck@gmail.com>
2023-07-26 15:16:36 -0700

    tas02: what is type?

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,6 @@
 def is_same_class(obj, a_class):
     """Function that returns True/False if obj is a type of a_class
     """
-    return  isinstance(obj, a_class)
+
+
+    return type(obj) is a_class
