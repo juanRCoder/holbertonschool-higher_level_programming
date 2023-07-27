c70c858b472b4f54c60c34ba6905aaeacf647b61
juanRCoder <guillecarck@gmail.com>
2023-07-26 20:17:32 -0700

    task06: function area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """" class BaseGeometry """
+    def area(self):
+        """function has area of figure geometry
+        """
+        raise Exception("area() is not implemented")
