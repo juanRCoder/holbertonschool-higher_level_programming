6a3774c4ab3edea468db99c441b648ee2c37c4fd
juanRCoder <guillecarck@gmail.com>
2023-07-28 15:30:29 -0700

    task08: class rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module has a class Rectangle that inherit from BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """" class BaseGeometry """
+    def area(self):
+        """function has area of figure geometry
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that validation if value is a number integer
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle """
+    def __init__(self, width, height):
+        """function init that inicialitation width and height
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
