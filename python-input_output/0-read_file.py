399dc670c3548f71f48db20275953caad4475447
juanRCoder <guillecarck@gmail.com>
2023-07-29 14:04:12 -0700

    task00: readFile

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has a function that print a text alone read
+"""
+
+
+def read_file(filename=""):
+    """ function that reads a text file """
+    with open(filename, "r", encoding="utf-8") as fileText:
+        content = fileText.read()
+        print(content)
+        
