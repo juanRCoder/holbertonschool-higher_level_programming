c529560af8b49ef979eebd5f5a2a541eae417247
juanRCoder <guillecarck@gmail.com>
2023-07-29 17:04:48 -0700

    task03: str_json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has function to json_string
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ function that return a JSON """
+    return json.dumps(my_obj)
