bcb49af6a34356131991c634a8aa224df521fb45
juanRCoder <guillecarck@gmail.com>
2023-08-19 14:43:41 -0700

    Feat: list all rows if the table 'first_table'

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,4 @@
--- list all tables of database 'hbtn_0c_0'
-USE hbtn_0c_0;
+-- select the database 'hbtn_0c_0'.
+-- USE hbtn_0c_0.
+-- list all rows if the table 'first_table'
 SELECT * FROM first_table;
