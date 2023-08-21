0e8d65c6d52bf8b2ae97e1a69a691ea1c719d42c
juanRCoder <guillecarck@gmail.com>
2023-08-21 16:48:36 -0700

    Feat: create Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create table cities in hbtn_0c_usa.
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
