7ff125418df7706506a0bfa1daf1c47973c004a1
juanRCoder <guillecarck@gmail.com>
2023-08-21 17:29:00 -0700

    Feat: list all cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- list all cities of the database.
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name 
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
