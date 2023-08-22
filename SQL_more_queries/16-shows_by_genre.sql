62336fabc63d744dba7bff066e6b7c3d80a812a8
juanRCoder <guillecarck@gmail.com>
2023-08-22 10:51:38 -0700

    Feat: Script that lists all shows and all genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres 
 	ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows 
+LEFT JOIN tv_shows 
 	ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
