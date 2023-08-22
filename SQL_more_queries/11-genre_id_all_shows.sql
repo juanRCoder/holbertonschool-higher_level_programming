4552415e28bcf7d2fb3aab71e50ff2da60a77572
juanRCoder <guillecarck@gmail.com>
2023-08-22 08:51:18 -0700

    Feat: if a show doesn't have a genre, display NULL

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- list all show contained in 'hbtn_0d_tvshows'.
-SELECT tv_shows.title AS title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id  AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, genre_id ASC;
