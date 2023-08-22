18dfb1f29e9c7f5401afbd8f5ee561e1c2886fdd
juanRCoder <guillecarck@gmail.com>
2023-08-22 08:30:36 -0700

    Feat: Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- list all show contained in 'hbtn_0d_tvshows'.
-SELECT tv_shows.title AS title, tv_show_genres.genre_id  AS genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = genre_id
 ORDER BY title, genre_id ASC;
