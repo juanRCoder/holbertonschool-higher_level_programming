42077fbe6183c2bab3cd15ae7cdcd7a863e2aa30
juanRCoder <guillecarck@gmail.com>
2023-08-22 12:22:40 -0700

    Feat: Write a script that list genres that not belong to a show

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,13 @@
 -- Script that lists all shows and all genres.
 SELECT tv_genres.name
 FROM tv_genres
-
-LEFT JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-	ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
-
-WHERE tv_shows.id IS NULL
-
+WHERE tv_genres.name NOT IN (
+	SELECT tv_genres.name
+	FROM tv_genres
+	LEFT JOIN tv_show_genres
+		ON tv_genres.id = tv_show_genres.genre_id
+	LEFT JOIN tv_shows
+		ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_show.title = 'Dexter')
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name ASC;
