3685d00e340c6c18ac50bd2e733e8e4039f68ae2
juanRCoder <guillecarck@gmail.com>
2023-08-24 16:34:36 -0700

    Feat: Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Lists all states that matches the argument 4"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    search_name = sys.argv[4]
+    HOST = 'localhost'
+
+    db = MySQLdb.connect(host=HOST, user=user, passwd=passwd, db=db, port=3306)
+
+    pointer = db.cursor()
+    query = ("SELECT id, name FROM states \
+            WHERE name = %s \
+            ORDER BY id ASC; ")
+    pointer.execute(query, (search_name,))
+    states = pointer.fetchall()
+
+    for state in states:
+        print(state)
+
+    pointer.close()
+    db.close()
