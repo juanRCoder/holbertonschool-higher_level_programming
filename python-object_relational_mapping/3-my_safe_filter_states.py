d7246aaefc92c814be1ae8cfca0bb35dfa620942
juanRCoder <guillecarck@gmail.com>
2023-08-24 16:51:31 -0700

    Feat: file that is safe from MySQL injections!

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Lists all states that matches the argument 4"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    search_name = sys.argv[4]
+    HOST = 'localhost'
+
+    db = MySQLdb.connect(host=HOST, user=user, passwd=passwd, db=db, port=3306)
+
+    pointer = db.cursor()
+    query = ("SELECT id, name FROM states \
+    WHERE name = '{}' \
+    ORDER BY id ASC; ".format(search_name))
+    pointer.execute(query)
+    states = pointer.fetchall()
+
+    for state in states:
+        if state[1][0] == 'N':
+            print(state)
+
+    pointer.close()
+    db.close()
