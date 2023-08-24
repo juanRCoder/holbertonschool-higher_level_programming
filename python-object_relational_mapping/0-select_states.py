8471e037dba736aef73f068f635eb48129c8dd30
juanRCoder <guillecarck@gmail.com>
2023-08-24 14:04:29 -0700

    Feat: Get all states from mySQL

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa
-"""
+""" Lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
-aa
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306)
 
-    pointer = db.cursor()
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    database_mySQL = MySQLdb.connect(user, password, db, port=3306)
+
+    pointer = database_mySQL.cursor()
     pointer.execute("SELECT * FROM states;")
     states = pointer.fetchall()
 
