87a1dbf34664b84ce1fa147fb5fa72f9eca60284
juanRCoder <guillecarck@gmail.com>
2023-08-24 14:19:53 -0700

    Feat: connect with mysql and select data

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,13 @@ import MySQLdb
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    password = sys.argv[2]
+    passwd = sys.argv[2]
     db = sys.argv[3]
+    HOST = 'localhost'
 
-    database_mySQL = MySQLdb.connect(user, password, db, port=3306)
+    db = MySQLdb.connect(host=HOST, user=user, passwd=passwd, db=db, port=3306)
 
-    pointer = database_mySQL.cursor()
+    pointer = db.cursor()
     pointer.execute("SELECT * FROM states;")
     states = pointer.fetchall()
 
