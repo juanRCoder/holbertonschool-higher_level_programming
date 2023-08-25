cd6c0d8899719419eadb43d2436761cfc5541835
juanRCoder <guillecarck@gmail.com>
2023-08-24 21:43:26 -0700

    Feat: connect a mysql with SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa """
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    dbSQL = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(dbSQL.format(user, passwd, db), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
