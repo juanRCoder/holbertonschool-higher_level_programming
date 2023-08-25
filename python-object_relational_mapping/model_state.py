5a31957e90770c97812ed14071538e0c098f9f41
juanRCoder <guillecarck@gmail.com>
2023-08-24 21:14:08 -0700

    Feat: First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" python file that contains the class definition of a State and an instance 
+""" python file that contains the class definition of a State and an instance
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,9 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
-    """ Class state that inherient of the Base""" 
+    """ Class state that inherient of the Base"""
     __tablename__ = "states"
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
