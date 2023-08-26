0b0acc57bfbfe97e1306ba03deee4b768013c54a
juanRCoder <guillecarck@gmail.com>
2023-08-25 20:20:49 -0700

    Feat: delete states that contain letter 'a'

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,11 +18,10 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
     for state in states:
-        if 'a' in state.name:
-            session.delete(state)
+        session.delete(state)
 
     session.commit()
     session.close()
