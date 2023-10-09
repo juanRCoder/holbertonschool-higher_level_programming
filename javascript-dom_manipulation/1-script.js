a58b6e6ada61d42c1d0717c6950382983d39f5f2
juanRCoder <guillecarck@gmail.com>
2023-10-09 22:33:03 +0000

    feat: manipulation tag HTML with javascript: (DOM)

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('header');
+let btnHeader = document.getElementById('red_header');
+
+btnHeader.addEventListener('click',()=>(
+	header.style.color = '#FF0000';
+});
+//header.style.color = '#FF0000';
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('header');
+let btnHeader = document.getElementById('red_header');
+
+btnHeader.addEventListener('click',()=>{
+	header.classList.add('red');
+});
