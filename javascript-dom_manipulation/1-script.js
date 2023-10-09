aef92c1a6d8706a26e64e950345f1dfffae9a766
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2023-10-09 17:37:10 -0500

    feat: fixing typo in () with {}

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,7 +1,7 @@
 let header = document.querySelector('header');
 let btnHeader = document.getElementById('red_header');
 
-btnHeader.addEventListener('click',()=>(
+btnHeader.addEventListener('click',()=>{
 	header.style.color = '#FF0000';
 });
 //header.style.color = '#FF0000';
