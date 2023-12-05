52ff3223b76d761979ef19c09d1183837bdb0043
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2023-12-05 11:09:43 -0500

    Delete python-data_structures directory

--- python-data_structures/0-main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-my_lista = [1, 2, 3, 4, 5, 6]
-print_list_integer(my_lista)
--- python-data_structures/8-multiple_returns.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    len_str = len(sentence)
-    if len_str == 0:
-        first_character = None
-    else:
-        first_character = sentence[0]
-    return len_str, first_character
