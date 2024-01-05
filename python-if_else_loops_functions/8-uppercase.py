c353548d39ce4ed05ed2607df339ffce3deb28bb
juanRCoder <guillecarck.com>
2024-01-05 03:59:29 +0000

    feat: convert character minus a character plus

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    string = ''
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            string += chr(ord(i) - 32)
+        else:
+            string += i
+    print("{}".format(string))
