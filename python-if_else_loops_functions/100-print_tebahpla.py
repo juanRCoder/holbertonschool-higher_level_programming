a0f4dc9ee1a5efe4032e0c13ee5fa5eb6b3a70e9
juanRCoder <guillecarck@gmail.com>
2024-01-05 11:34:30 -0500

    feat: modify shebang for execute of file

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-for l in range(122, 96, -1):
-    print("{}".format(chr(l)) if l % 2 == 0 else chr(l - 32), end="")
+#!/usr/bin/env python3
+for i in range(122, 96, -1):
+    print("{}".format(chr(i)) if i % 2 == 0 else chr(i - 32), end="")
