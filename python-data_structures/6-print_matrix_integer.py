64a000a978876f0a3a092351a8328d619466d990
juanRCoder <guillecack@gmail.com>
2024-01-07 01:00:14 +0000

    fix: remove space in last element of the list

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lista in matrix:
-        for i in lista:
-            print("{:d}".format(i), end=" ")
+        for i, v in enumerate(lista):
+            if i != len(lista) - 1:
+                print("{:d}".format(v), end=" ")
+            else:
+                print("{:d}".format(v), end="")
         print()
