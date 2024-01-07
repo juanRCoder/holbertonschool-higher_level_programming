afa55e7d9fc1213fe68a7cdf2f34254d0f74391f
juanRCoder <guillecarck@gmail.com>
2024-01-07 04:17:58 +0000

    fix: check index of the end range

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 1:
-        tuple_a += (0, 0,)
-    elif len(tuple_b) < 2:
-        tuple_a += (0,)
-
-    if len(tuple_b) < 1:
-        tuple_b += (0, 0,)
-    elif len(tuple_b) < 2:
-        tuple_b += (0,)
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
 
     a = tuple_a[0] + tuple_b[0]
     b = tuple_a[1] + tuple_b[1]
