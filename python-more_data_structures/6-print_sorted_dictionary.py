4487177cd105b629046e14c059ea0cc1b198337a
juanRCoder <guillecarck@gmail.com>
2024-01-09 18:47:41 -0500

    feat: delete key in dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    order = sorted(a_dictionary.keys())
-    for k in order:
-        print(f"{k}: {a_dictionary[k]}")
+    if isinstance(a_dictionary, dict):
+        order = sorted(a_dictionary.keys())
+        for k in order:
+            print(f"{k}: {a_dictionary[k]}")
+    else:
+        pass
