d9705046e462f8290963e1c22c873e5c17956762
juanRCoder <guillecarck@gmail.com>
2024-01-09 17:25:35 -0500

    feat: transform roman at integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    value = 0
+    prev = 0
+    for i in reversed(roman_string):
+        current = romans[i]
+        if current < prev:
+            value -= current
+        else:
+            value += current
+
+        prev = current
+
+    return value
