892ac914132eb4467283ac73a1344e1bb64cbfbc
juanRCoder <guillecarck@gmail.com>
2024-01-13 14:48:46 -0500

    feat: check if isinstance obj from a_class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module that contains a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Function that returns True if the object is an instance"""
+    return isinstance(obj, a_class)
