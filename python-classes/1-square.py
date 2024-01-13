46ab33263558e8b8165146a468ffb254366239e0
juanRCoder <guillecarck@gmail.com>
2024-01-12 22:41:01 -0500

    feat: add constructor with private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contain class empty"""
+
+
+class Square:
+    """ class square"""
+    def __init__(self, size):
+        """ Constructor of the class"""
+        self.__size = size
