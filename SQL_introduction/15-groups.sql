e797ac40db739088ba2f12733bbafa84445789df
juanRCoder <guillecarck@gmail.com>
2024-01-15 12:39:42 -0500

    fix: fixing count all records not score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- list the number of record with the same score.
-SELECT score, COUNT(score) AS number FROM second_table
+SELECT score, COUNT(*) AS number FROM second_table
 GROUP BY score
 ORDER BY score DESC;
