23ea353124756d773194c6ef37e9225c75aabb07
juanRCoder <guillecarck@gmail.com>
2024-01-15 11:21:28 -0500

    fix: TYPO in name jhon for john

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,9 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
--- insert values in table created
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'Jhon', 10),
+VALUES (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
