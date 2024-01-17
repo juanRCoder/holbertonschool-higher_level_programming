ae258551fcb2c7834a9c1b7b8ccd7827260f9c45
juanRCoder <guillecarck@gmail.com>
2024-01-16 19:49:49 -0500

    feat: list all shows without the genre Comedy

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- lists all shows without the genre Comedy.
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id 
+    FROM tv_show_genres
+    RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = "Comedy" 
+)
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC;
