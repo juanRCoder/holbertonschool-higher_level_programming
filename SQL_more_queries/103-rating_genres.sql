6ed8c46c8fd3b270b2e99d0ef275ebae7e3daa9c
juanRCoder <guillecarck@gmail.com>
2024-01-16 20:08:08 -0500

    feat: lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -3,6 +3,6 @@ SELECT tv_genres.name, COALESCE(SUM(tv_show_ratings.rate), 0) AS 'rating'
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_shows ON  tv_shows.id = tv_show_genres.show_id
-RIGHT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
