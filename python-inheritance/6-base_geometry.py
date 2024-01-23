3e030f0479e8523b55a11441206105ed717bbf61
juanRCoder <guillecarck@gmail.com>
2024-01-23 11:12:14 -0500

    feat: add class new public instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that contain class empty"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """ function that display area of the attributes"""
+        raise Exception("area() is not implemented")
