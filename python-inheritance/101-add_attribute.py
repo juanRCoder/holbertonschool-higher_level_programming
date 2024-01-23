ea08732e84f3084a2e7677961700ffd6b5c1c7ae
juanRCoder <guillecarck@gmail.com>
2024-01-23 13:16:26 -0500

    feat: add new attribute if is posible

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/ust/bin/python3
+"""method that contain an function"""
+
+
+def add_attribute(obj, att, value):
+    """ add new attribute if is posible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, att, value)
