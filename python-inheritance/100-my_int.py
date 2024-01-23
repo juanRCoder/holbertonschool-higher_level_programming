3035ff220c661645b2a7b805408b7f3daf679e98
juanRCoder <guillecarck@gmail.com>
2024-01-23 13:11:54 -0500

    fix: pycodestyle

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ method that contain an class"""
 
+
 class MyInt(int):
+    """class MyInt"""
+
     def __init__(self, num):
         """attributes of the class"""
         self._num = num
-    
+
     def __eq__(self, other):
         """method rebeld if attribute is not equal"""
         return self._num != other
-    
+
     def __ne__(self, other):
         """method rebeld if attribute is equal"""
         return self._num == other
