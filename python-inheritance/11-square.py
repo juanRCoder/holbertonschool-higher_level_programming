88f7ec964c75a39d40b2fd112af8c54f42da2a8e
juanRCoder <guillecarck@gmail.com>
2024-01-23 12:49:34 -0500

    feat: modify special methos str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module that contain class BaseGeometry"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """check if size is integer validator"""
+        self.integer_validator("size", size)
+        self._size = size
+        super().__init__(self._size, self._size)
+
+    def area(self):
+        """method for display area of the square"""
+        return super().area()
+
+    def __str__(self):
+        """special method for information"""
+        return f"[Square] {self._size}/{self._size}"
