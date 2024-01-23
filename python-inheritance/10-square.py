b000c84a1e3fbb706acf9de7218e4a4546ed0869
juanRCoder <guillecarck@gmail.com>
2024-01-23 12:44:01 -0500

    fix: inherient rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """module that contain class BaseGeometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """class Square"""
     def __init__(self, size):
         """check if size is integer validator"""
         self.integer_validator("size", size)
         self._size = size
-
-    def __str__(self):
-        """special method for information"""
-        return f"[Rectangle] {self._size}/{self._size}"
+        super().__init__(self._size, self._size)
 
     def area(self):
         """method for display area of the square"""
-        return self._size * self._size
+        return super().area()
