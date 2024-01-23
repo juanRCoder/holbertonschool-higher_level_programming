26f3392b04a616a49ad53bbe237e2729e84cbf28
juanRCoder <guillecarck@gmail.com>
2024-01-23 11:38:13 -0500

    feat: add new method for integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module that contain class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """function that display area of the attributes"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that integer validator"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
