468957e87f2553e8a95e3bb8dc5a477c33358316
juanRCoder <guillecarck@gmail.com>
2024-01-25 10:06:40 -0500

    feat: return length of the text

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that contain a function"""
+
+
+def write_file(filename="", text=""):
+    """function that read and count length characteres"""
+    with open(filename, 'w', encoding="UTF-8") as file:
+        data_length = file.write(text)
+    
+    return data_length
