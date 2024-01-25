165642a86b2a6ddcc9fbf35f6f854fcdb195dbda
juanRCoder <guillecarck@gmail.com>
2024-01-25 10:10:37 -0500

    fix: whitspace, pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ def write_file(filename="", text=""):
     """function that read and count length characteres"""
     with open(filename, 'w', encoding="UTF-8") as file:
         data_length = file.write(text)
-    
     return data_length
